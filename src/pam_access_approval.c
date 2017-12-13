10cbd8ac4923d92ed0783afe426d524b51fa0577
osiutino <osiutino@gmail.com>
2017-12-14 01:32:59 +0800

    cmake support

--- src/pam_access_approval.c
+++ src/pam_access_approval.c
@@ -6,14 +6,15 @@
 
 #include <security/pam_modules.h>
 #include <security/pam_appl.h>
+#include "src/config.h"
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
-	printf("access_approval_pam - authenticate\n");
+	printf("access_approval_pam (v%d.%d) - authenticate\n", ACCESS_APPROVAL_PAM_VERSION_MAJOR, ACCESS_APPROVAL_PAM_VERSION_MINOR);
 	return PAM_SUCCESS;
 }
 
 
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	printf("access_approval_pam - open_session\n");
+	printf("access_approval_pam (v%d.%d) - open_session\n", ACCESS_APPROVAL_PAM_VERSION_MAJOR, ACCESS_APPROVAL_PAM_VERSION_MINOR);
 	return PAM_SUCCESS;
 }
