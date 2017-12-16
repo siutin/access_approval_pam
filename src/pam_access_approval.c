8c35e79b7c65b0290d0e35c22640ee9626a75e9c
osiutino <osiutino@gmail.com>
2017-12-16 18:52:44 +0800

    msleep

--- src/pam_access_approval.c
+++ src/pam_access_approval.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
+#include <time.h>
 
 #define PAM_SM_AUTH
 
 #include <security/pam_modules.h>
 #include <security/pam_appl.h>
 #include "src/config.h"
+
+void msleep(uint32_t milisec)
+{
+    struct timespec req={0};
+    time_t sec=(uint32_t)(milisec/1000);
+    milisec=milisec-(sec*1000);
+    req.tv_sec=sec;
+    req.tv_nsec=milisec*1000000L;
+    while(nanosleep(&req,&req)==-1)
+         continue;
+}
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
 	printf("access_approval_pam (v%d.%d) - authenticate\n", ACCESS_APPROVAL_PAM_VERSION_MAJOR, ACCESS_APPROVAL_PAM_VERSION_MINOR);
