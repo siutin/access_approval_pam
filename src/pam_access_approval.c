55db8e32eb66a12546b459cf4566691d96aebe66
osiutino <osiutino@gmail.com>
2017-12-19 02:47:11 +0800

    inotify implementation using fswatch

--- src/pam_access_approval.c
+++ src/pam_access_approval.c
@@ -22,6 +22,10 @@ void msleep(uint32_t milisec)
          continue;
 }
 
+void session_handler(fsw_cevent const * const events, const unsigned int event_num, void * data) {
+  printf("session_handler: %d\n",event_num);
+}
+
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
 	printf("access_approval_pam (v%d.%d) - authenticate\n", ACCESS_APPROVAL_PAM_VERSION_MAJOR, ACCESS_APPROVAL_PAM_VERSION_MINOR);
 	// Initialize the library
@@ -32,6 +36,34 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
 	{
 		return PAM_SYSTEM_ERR;
 	}
+
+	const FSW_HANDLE handle = fsw_init_session(inotify_monitor_type);
+
+	if (FSW_INVALID_HANDLE == handle) {
+		fsw_last_error();
+		(void)printf("Invalid fswatch handle: %d\n",handle);
+	}
+
+	if ( FSW_OK != fsw_add_path(handle, "/tmp/test/notify-dir/notify-file.txt") ) {
+		fsw_last_error();
+		(void)printf("Fail to fsw_add_path");
+	}
+
+	if(FSW_OK != fsw_set_callback(handle, session_handler, NULL)) {
+		fsw_last_error();
+		(void)printf("Fail to fsw_set_callback");
+	}
+
+	fsw_set_allow_overflow(handle,0);
+
+	if(FSW_OK != fsw_start_monitor(handle)) {
+  	fsw_last_error();
+		(void)printf("Fail to fsw_start_monitor");
+	}
+
+	while (1) {
+		msleep(1000);
+	};
 	return PAM_SUCCESS;
 }
 
