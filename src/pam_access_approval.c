fe80227d360b634e685c2f85fdbf5c94780f65db
osiutino <osiutino@gmail.com>
2017-12-19 02:47:48 +0800

    beautify

--- src/pam_access_approval.c
+++ src/pam_access_approval.c
@@ -13,62 +13,62 @@
 
 void msleep(uint32_t milisec)
 {
-    struct timespec req={0};
-    time_t sec=(uint32_t)(milisec/1000);
-    milisec=milisec-(sec*1000);
-    req.tv_sec=sec;
-    req.tv_nsec=milisec*1000000L;
-    while(nanosleep(&req,&req)==-1)
-         continue;
+								struct timespec req={0};
+								time_t sec=(uint32_t)(milisec/1000);
+								milisec=milisec-(sec*1000);
+								req.tv_sec=sec;
+								req.tv_nsec=milisec*1000000L;
+								while(nanosleep(&req,&req)==-1)
+																continue;
 }
 
 void session_handler(fsw_cevent const * const events, const unsigned int event_num, void * data) {
-  printf("session_handler: %d\n",event_num);
+								printf("session_handler: %d\n",event_num);
 }
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
-	printf("access_approval_pam (v%d.%d) - authenticate\n", ACCESS_APPROVAL_PAM_VERSION_MAJOR, ACCESS_APPROVAL_PAM_VERSION_MINOR);
-	// Initialize the library
-	FSW_STATUS ret = fsw_init_library();
-	printf("fsw_init_library: %d\n", ret);
+								printf("access_approval_pam (v%d.%d) - authenticate\n", ACCESS_APPROVAL_PAM_VERSION_MAJOR, ACCESS_APPROVAL_PAM_VERSION_MINOR);
+								// Initialize the library
+								FSW_STATUS ret = fsw_init_library();
+								printf("fsw_init_library: %d\n", ret);
 
-	if (ret != FSW_OK)
-	{
-		return PAM_SYSTEM_ERR;
-	}
+								if (ret != FSW_OK)
+								{
+																return PAM_SYSTEM_ERR;
+								}
 
-	const FSW_HANDLE handle = fsw_init_session(inotify_monitor_type);
+								const FSW_HANDLE handle = fsw_init_session(inotify_monitor_type);
 
-	if (FSW_INVALID_HANDLE == handle) {
-		fsw_last_error();
-		(void)printf("Invalid fswatch handle: %d\n",handle);
-	}
+								if (FSW_INVALID_HANDLE == handle) {
+																fsw_last_error();
+																(void)printf("Invalid fswatch handle: %d\n",handle);
+								}
 
-	if ( FSW_OK != fsw_add_path(handle, "/tmp/test/notify-dir/notify-file.txt") ) {
-		fsw_last_error();
-		(void)printf("Fail to fsw_add_path");
-	}
+								if ( FSW_OK != fsw_add_path(handle, "/tmp/test/notify-dir/notify-file.txt") ) {
+																fsw_last_error();
+																(void)printf("Fail to fsw_add_path");
+								}
 
-	if(FSW_OK != fsw_set_callback(handle, session_handler, NULL)) {
-		fsw_last_error();
-		(void)printf("Fail to fsw_set_callback");
-	}
+								if(FSW_OK != fsw_set_callback(handle, session_handler, NULL)) {
+																fsw_last_error();
+																(void)printf("Fail to fsw_set_callback");
+								}
 
-	fsw_set_allow_overflow(handle,0);
+								fsw_set_allow_overflow(handle,0);
 
-	if(FSW_OK != fsw_start_monitor(handle)) {
-  	fsw_last_error();
-		(void)printf("Fail to fsw_start_monitor");
-	}
+								if(FSW_OK != fsw_start_monitor(handle)) {
+																fsw_last_error();
+																(void)printf("Fail to fsw_start_monitor");
+								}
 
-	while (1) {
-		msleep(1000);
-	};
-	return PAM_SUCCESS;
+								while (1) {
+																msleep(1000);
+								};
+								return PAM_SUCCESS;
 }
 
 
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	printf("access_approval_pam (v%d.%d) - open_session\n", ACCESS_APPROVAL_PAM_VERSION_MAJOR, ACCESS_APPROVAL_PAM_VERSION_MINOR);
-	return PAM_SUCCESS;
+								printf("access_approval_pam (v%d.%d) - open_session\n", ACCESS_APPROVAL_PAM_VERSION_MAJOR, ACCESS_APPROVAL_PAM_VERSION_MINOR);
+								return PAM_SUCCESS;
 }
